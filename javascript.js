d3bc5e08e5326c73c1b353e0c38969e563b29094
Niko Gang <niko_gang@hotmail.com>
2017-11-29 23:21:32 +0100

    update2

--- javascript.js
+++ javascript.js
@@ -1,32 +1,13 @@
-// Get Niko's image and insert it in the modal
-var modal = document.getElementById("myModal");
-var img = document.getElementById("nikopic");
-var modalImg = document.getElementById("nikoimg");
-var captionText = document.getElementById("caption");
-if (img) {
-    img.onclick = function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-}
-// Get the close element
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-//Close modal with X
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Close modal with window click
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal.style.display = "none";
-    }
+$(() => {
+function openOverlay(a, b) {
+  singleToggle(a, b);
+  singleToggle(b, b);
 };
+
+openOverlay($())
+
+
+
 // API PART not done yet!!! But to show you what is going on
 let results = [];
 
@@ -104,3 +85,4 @@ function search(searchWord) {
         }
     });
 };
+});
