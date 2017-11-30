7339fca7f2ff2e33159e64c3d383248ca263a956
Niko Gang <niko_gang@hotmail.com>
2017-11-30 15:55:05 +0100

    cookie added

--- javascript.js
+++ javascript.js
@@ -31,45 +31,8 @@ $(() => {
         };
     });
 
-    // Get Niko's image and insert it in the modal
-    var modal = document.getElementById("myModal");
-    var img = document.getElementById("nikopic");
-    var modalImg = document.getElementById("nikoimg");
-    var captionText = document.getElementById("caption");
-    if (img) {
-        img.onclick = function() {
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
-        }
-    }
-    // Get the close element
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+    $("#cookiebtn").on("click", () => {
+      $("#cookieoverlayid").hide();
+    });
 
-    $(() => {
-
-
-        function openOverlay(a, b) {
-            singleToggle(a, b);
-        }
-
-
-        // Close modal with window click
-        window.onclick = function(event) {
-
-            if (event.target == modal1) {
-                modal.style.display = "none";
-            }
-        }
-    })
-    if (event.target == modal1) {
-        modal.style.display = "none";
-    }
 });
-
-openOverlay($('#nikog'), $("#overlaynikoid"));
-
-$("#nikog").slideToggle();
